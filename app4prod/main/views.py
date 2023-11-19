1e6dc83b7b091d9edc70559ef468d72ac37cd12c
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-11-19 15:14:07 +0100

    feat(application): :sparkles: hello page

--- app4prod/main/views.py
+++ app4prod/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello dear user!</h1>')
