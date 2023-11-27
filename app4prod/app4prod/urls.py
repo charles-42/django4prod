1c6dd4a30cc9d536a69350bdec3895ca947a6774
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-11-27 17:49:14 +0100

    feat(application): :sparkles: login system

--- app4prod/app4prod/urls.py
+++ app4prod/app4prod/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello)
+    path('', views.index,name="index"),
+    path("accounts/", include("django.contrib.auth.urls")), # intègre l'authentification
+    path('accounts/profile/', views.hello,name="hello"), # redicrection après la page login
 ]
--- app4prod/main/views.py
+++ app4prod/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
 def hello(request):
-    return render(request, 'hello.html')
+    username = request.user.username
+    return render(request, 'hello.html', {'username': username})
