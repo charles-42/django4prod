4880330623a2bd0fce709cb26254f1adf980e112
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-11-27 13:20:54 +0100

    feat(application): :art: add  html template for hell page

--- app4prod/main/views.py
+++ app4prod/main/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
 def hello(request):
-    return HttpResponse('<h1>Hello dear user!</h1>')
+    return render(request, 'hello.html')
