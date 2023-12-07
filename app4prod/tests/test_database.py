f58a22bf684407845a2d299969abd9cb79b6ad99
charles-42 <71760090+charles-42@users.noreply.github.com>
2023-12-07 13:13:46 +0100

    style(tests): :art: test databse

--- app4prod/tests/test_database.py
+++ app4prod/tests/test_database.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_connexion():
-    first_object = User.objects.first
-    assert first_object is not None
+    first = User.objects.first()
+    assert first is not None
